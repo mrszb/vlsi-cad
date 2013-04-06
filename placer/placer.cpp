012af284767b5e97031aab4f0a746155404fd95c
Marian SZABO <maros>
2013-04-06 14:28:56 -0400

    initial commit

---
+++ placer/placer.cpp
@@ -0,0 +1,111 @@
+// placer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <assert.h>
+#include <valarray>
+#include "solver.h"
+
+///////////////////////////////////////////////////////////////////////
+
+typedef size_t ID_GATE;
+typedef size_t ID_NET;
+typedef std::map<ID_GATE, std::set<ID_NET> > NETLIST;
+
+typedef size_t ID_PAD;
+struct PAD
+{
+	ID_NET id_net;
+	double x, y;
+};
+
+typedef std::map<ID_PAD, PAD> PADLIST;
+
+//////////////////////////////////////////////////////////////////////
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::ifstream is(argv[1]);
+
+	size_t nof_gates, nof_nets;
+
+	is >> nof_gates;
+	is >> nof_nets;
+
+	NETLIST netlist;
+	PADLIST pads;
+
+	for (size_t i = 0; i < nof_gates; ++i)
+	{
+		ID_GATE gate;
+		is >> gate;
+
+		assert( gate > 0 && gate <= nof_gates);
+
+		size_t nof_nets_for_this_gate;
+		is >> nof_nets_for_this_gate;
+
+		for (size_t j = 0; j < nof_nets_for_this_gate; ++j)
+		{
+			ID_NET net;
+			is >> net;
+
+			assert( net > 0 && net <= nof_nets);
+			netlist[gate].insert(net);
+		}
+	}
+
+	size_t nof_pads;
+	is >> nof_pads;
+
+	for (size_t i = 0; i < nof_pads; ++i)
+	{
+		ID_PAD pinid;
+		is >> pinid;
+
+		assert( pinid > 0 && pinid <= nof_pads);
+
+		PAD pad;
+		is >> pad.id_net;
+		is >> pad.x;
+		is >> pad.y;
+
+		pads[pinid] = pad;
+	}
+
+	std::cout << pads.size() << std::endl;
+
+	coo_matrix A;
+	int R[]    = {0, 0, 1, 1, 1, 2, 2};
+	int C[]    = {0, 1, 0, 1, 2, 1, 2};
+	double V[] = {4.0, -1.0, -1.0,  4.0, -1.0, -1.0, 4.0};
+	A.n = 3;
+	A.nnz = sizeof(R) / sizeof(int);
+	A.row.resize(A.nnz);
+	A.col.resize(A.nnz);
+	A.dat.resize(A.nnz);
+
+	A.row = valarray<int>(R, A.nnz);
+	A.col = valarray<int>(C, A.nnz);
+	A.dat = valarray<double>(V, A.nnz);
+
+	// initialize as [1, 1, 1]
+	valarray<double> x(1.0, A.n);
+	valarray<double> b(A.n);
+	A.matvec(x, b); // b = Ax
+
+	cout << "b should equal [3,2,3]" << endl;
+	print_valarray(b);
+
+	// solve for x
+	cout << "x = " << endl;
+	A.solve(b, x);
+	print_valarray(x);
+
+	return 0;
+}
+
---
+++ unate_recursive_complement/vlsicad_urc.cpp
@@ -0,0 +1,412 @@
+// vlsicad_urc.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+///////////////////////////////////////////////////
+
+enum CUBEPTS
+{
+	XT      = 1,
+	XF   = 2,
+	XX       = 3
+};
+
+typedef std::vector<CUBEPTS> cubeline;
+typedef std::vector<cubeline> clist;
+
+///////////////////////////////////////////////////
+
+bool has_all_dont_care ( const cubeline& cl )
+{
+	for ( size_t i = 0; i < cl.size(); i++ )
+	{
+		if (cl[i] != XX)
+			return false;
+	}
+
+	return true;
+}
+
+struct cubeList
+{
+	size_t _nof_vars;
+	clist _l;
+
+	bool can_be_converted (cubeList& result) const;
+	size_t get_nof_vars (void) const { return _nof_vars; };
+	bool has_some_dont_care(void) const;
+
+	void read	( std::istream& is );
+	void write	( std::ostream& os );
+};
+
+////////////////////////////////////////////////////////
+
+bool cubeList::has_some_dont_care(void) const
+{
+	for (size_t i = 0; i < _l.size(); i++)
+	{
+		if ( has_all_dont_care(_l[i]) )
+			return true;
+	}
+
+	return false;
+}
+
+bool cubeList::can_be_converted(cubeList& result) const
+{
+	if ( _l.empty() )
+	{
+		// Bolean 0:
+		// return 1 [11 11 11 ...11]
+
+		cubeline clres;
+
+		for (size_t i = 0; i < get_nof_vars(); i++)
+			clres.push_back(XX);
+
+		result._l.push_back(clres);
+		return true;
+	}
+	else if (has_some_dont_care())
+	{
+		// Boolean T
+		// return empty list
+		return true;
+	}
+	else if (_l.size()==1)
+	{
+		const cubeline cline = _l[0];
+		// De Morgan
+
+		for (size_t i = 0; i < get_nof_vars(); i++)
+		{
+			cubeline clres;
+
+			switch ( cline[i] )
+			{
+				case XX:
+					break;
+
+				case XT:
+
+					for ( size_t j = 0; j < get_nof_vars(); j++ )
+						clres.push_back( (j == i) ?	XF : XX);
+					result._l.push_back(clres);
+					break;
+
+				case XF:
+
+					for ( size_t j = 0; j < get_nof_vars(); j++ )
+						clres.push_back( (j == i) ? XT : XX);
+					result._l.push_back(clres);
+
+					break;
+			}
+		}
+
+		return true;
+	}
+
+
+	return false;
+}
+
+void cubeList::read(std::istream& is)
+{
+	is >> _nof_vars;
+
+	int lines;
+	is >> lines;
+
+
+	_l.reserve(lines);
+
+	/////////////////////////////////////
+
+	while (lines--> 0)
+	{
+		cubeline ln;
+		ln.reserve( get_nof_vars() );
+
+		for (size_t i = 0; i < get_nof_vars(); ++i)
+			ln.push_back(XX);
+
+		size_t cnt;
+		is >> cnt;
+
+		for (size_t i = 0; i < cnt; ++i)
+		{
+			int var;
+			is >> var;
+
+			if (var < 0)
+				ln[-var-1] = XF;
+			else
+				ln[var-1] = XT;
+		}
+
+		_l.push_back(ln);
+	}
+}
+
+void cubeList::write (std::ostream& os)
+{
+	os << get_nof_vars() << std::endl;
+	os << _l.size() << std::endl;
+
+	for (size_t cubeno = 0; cubeno < _l.size(); ++cubeno)
+	{
+		const cubeline& cl = _l[ cubeno ];
+
+		std::vector<int> carevar;
+
+		for (size_t i = 0; i < get_nof_vars(); ++i)
+		{
+			switch (cl[i])
+			{
+
+				case XT:
+					carevar.push_back(i+1);
+					break;
+
+				case XF:
+					carevar.push_back(-(i+1));
+					break;
+
+				case XX:
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		os << carevar.size();
+		os << " ";
+
+		for (size_t i = 0; i < carevar.size(); ++i)
+		{
+			os << carevar[i];
+			os << " ";
+		}
+
+		os << std::endl;
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+struct binate {
+	size_t t;
+	size_t f;
+
+	binate ()
+	{
+		t = f = 0;
+	}
+
+	///////////////////////////////////////////
+
+	bool is_binate (void) const
+	{
+		return ( t != 0 && f != 0 );
+	}
+
+	size_t get_binates() const
+	{
+		return (is_binate()) ? t+f : 0;
+	}
+
+	size_t get_unate() const
+	{
+		return !is_binate() ? t+f : 0;
+	}
+
+	int get_tminc (void) const
+	{
+		return (t>f) ? t-f : f-t;
+	}
+};
+
+size_t find_split_index (const cubeList& cl )
+{
+	std::vector<binate> bincnt;
+	bincnt.resize( cl.get_nof_vars() );
+
+	for ( clist::const_iterator it = cl._l.begin();
+		it != cl._l.end(); ++it
+		)
+	{
+		const cubeline& cline = *it;
+		for (size_t ix = 0; ix < bincnt.size(); ++ix)
+		{
+			if (cline[ix] == XT)
+				bincnt[ix].t++;
+
+			else if (cline[ix] == XF)
+				bincnt[ix].f++;
+
+			//else if (cline[ix] == NOX)
+			//	bincnt[ix].n++;
+		}
+	}
+
+	std::vector<size_t> most_binate;
+	size_t max_binatness = 1;
+
+	std::vector<size_t> most_unate;
+	size_t max_unatness = 1;
+
+	for (size_t ix = 0; ix < bincnt.size(); ++ix)
+	{
+		const binate& b = bincnt[ix];
+		size_t mb = b.get_binates();
+		size_t mu = b.get_unate();
+
+		if (mb > max_binatness)
+		{
+			most_binate.clear();
+			max_binatness = mb;
+		}
+
+		if (mb == max_binatness)
+			most_binate.push_back(ix);
+
+		if (mu > max_unatness)
+		{
+			most_unate.clear();
+			max_unatness = mu;
+		}
+
+		if (mu == max_unatness)
+			most_unate.push_back(ix);
+	}
+
+	if (!most_binate.empty())
+	{
+		std::vector<size_t> min_tc;
+		int min_tc_val = max_binatness+3;
+
+		for (size_t i = 0; i < most_binate.size(); ++i)
+		{
+			size_t j = most_binate[i];
+			int tmic = bincnt[j].get_tminc();
+			if (tmic < min_tc_val)
+			{
+				min_tc.clear();
+				min_tc_val = tmic;
+			}
+
+			if (min_tc_val == tmic)
+				min_tc.push_back(j);
+		}
+
+		return *min_tc.begin();
+	}
+	else
+	{
+		return *most_unate.begin();
+	}
+
+	return 0;
+}
+
+cubeList cofactor(const cubeList& cl, size_t x, bool positive)
+{
+	cubeList res = cl;
+
+	for ( clist::iterator it = res._l.begin();
+		it != res._l.end();
+		)
+	{
+		cubeline& cline = *it;
+		if ( (cline[x] == XT && positive ) || (cline[x] == XF && !positive) )
+		{
+			cline[x] = XX;
+		}
+		else if (cline[x] != XX)
+		{
+			it = res._l.erase(it);
+			continue;
+		}
+		it++;
+	}
+
+
+	return res;
+}
+
+cubeList or (const cubeList& cl1, const cubeList& cl2)
+{
+	cubeList res = cl1;
+
+	for (size_t i = 0; i < cl2._l.size(); ++i)
+	{
+		const cubeline& cl = cl2._l[i];
+		res._l.push_back(cl);
+	}
+
+	return res;
+}
+
+cubeList and(const cubeList& cl, size_t x, bool positive)
+{
+	cubeList res = cl;
+
+	for (size_t i = 0; i < res._l.size(); ++i)
+	{
+		cubeline& clline = res._l[i];
+
+		clline[x] = positive ? XT : XF;
+	}
+
+	return res;
+}
+
+cubeList Complement (const cubeList& F)
+{
+	cubeList res;
+	res._nof_vars = F.get_nof_vars();
+
+	if (F.can_be_converted(res))
+		return res;
+	else
+	{
+		size_t x = find_split_index(F);
+
+		cubeList P = Complement(cofactor(F,x, true));
+		cubeList N = Complement(cofactor(F,x, false));
+
+		P = and( P, x, true);
+		N = and( N, x, false);
+
+		return or (P,N);
+
+	}
+
+	return res;
+}
+
+////////////////////////////////////////////////////////////////
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::ifstream in(argv[1]);
+
+	cubeList cl;
+	cl.read(in);
+
+	cubeList clres = Complement(cl);
+
+
+	//cl.write (std::cout);
+	clres.write (std::cout);
+	return 0;
+}
+
