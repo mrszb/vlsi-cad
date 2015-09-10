ae8a006dd2ed15b94118e05572a421af553b3937
Marian SZABO <maros>
2015-09-10 19:32:32 -0400

    updated boost version

--- cmake_generate_msvc_ide.bat
+++ cmake_generate_msvc_ide.bat
@@ -1,11 +1,11 @@
 mkdir .msvc_ide
 
 cd .msvc_ide
-SET BOOST_VER=1.56.0
+SET BOOST_VER=1.59.0
 
 SET BOOST_LIBRARYDIR=C:\MR\Boost\%BOOST_VER%\lib\msvc-12.0\x64
 SET BOOST_INCLUDEDIR=C:\MR\Boost\%BOOST_VER%\include
 
-%MR_ROOT%\CMake\3.0.1\bin\cmake -G "Visual Studio 12 Win64" ..
+%MR_ROOT%\CMake\3.3.1\bin\cmake -G "Visual Studio 12 Win64" ..
 
 cd ..
