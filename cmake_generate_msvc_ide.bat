92ce57e378cd7fa2cbb1d583166632c9adbc1e20
Marian SZABO <maros>
2017-12-21 01:24:52 -0500

    updated for boost 1.65 and VS2017

--- cmake_generate_msvc_ide.bat
+++ cmake_generate_msvc_ide.bat
@@ -1,11 +1,15 @@
 mkdir .msvc_ide
 
 cd .msvc_ide
-SET BOOST_VER=1.59.0
+SET BOOST_VER=1.65.0
+SET CMAKE_VER=3.10.1
 
-SET BOOST_LIBRARYDIR=C:\MR\Boost\%BOOST_VER%\lib\msvc-12.0\x64
-SET BOOST_INCLUDEDIR=C:\MR\Boost\%BOOST_VER%\include
+SET USER_LIBS=%USERPROFILE%\libraries
 
-%MR_ROOT%\CMake\3.3.1\bin\cmake -G "Visual Studio 12 Win64" ..
+SET BOOST_LIBRARYDIR=%USER_LIBS%\Boost\%BOOST_VER%\lib\msvc-14.1\x64
+SET BOOST_INCLUDEDIR=%USER_LIBS%\Boost\%BOOST_VER%\include
+
+REM %USER_LIBS%\CMake\%CMAKE_VER%\bin\cmake -G "Visual Studio 14 2015 Win64" ..
+%USER_LIBS%\CMake\%CMAKE_VER%\bin\cmake -G "Visual Studio 15 2017 Win64" ..
 
 cd ..
